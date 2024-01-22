353d4f5a8d82453fa6cc6d960d4960fc94b6a6ab
Code Moves <codemtutorials@gmail.com>
2024-01-22 15:49:56 +0330

    Add initial code sample

---
+++ 3-the-hard-stuff/08-undefined-vs-null/null.js
@@ -0,0 +1,16 @@
+
+//
+// Code Moves - null - Sample code 1
+//
+
+let name = null; 
+console.log(name); 
+
+function getUsersAge(user) { 
+  // get user's age 
+  // if not available return null 
+  return null;
+}
+console.log(getUsersAge("simon"));
+
+console.log(typeof null);
---
+++ 3-the-hard-stuff/08-undefined-vs-null/undefined.js
@@ -0,0 +1,22 @@
+
+//
+// Code Moves - undefined - Sample code 1
+//
+
+let name; 
+console.log(name); 
+
+function myFunction() { 
+  // do function stuff
+  // return nothing
+}
+console.log(myFunction());
+
+let myArray = [1, 7, 2, 3]; 
+console.log(myArray[100]);
+
+let myObject = { "age": "24" }; 
+console.log(myObject.name); 
+
+console.log(typeof undefined);
+
